b3fa0632cd2474ea32eb29a7e2d4da1a794714ae
riyad-sudo <riyad114022@gmail.com>
2024-05-21 20:16:53 +0200

    added widget page for differnt index case for navigation rail child

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+}
+
     return Scaffold(
       body: Row(
         children: [
@@ -86,7 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
+              child: page,
             ),
           ),
         ],
